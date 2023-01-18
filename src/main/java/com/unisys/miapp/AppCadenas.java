c76f688c9198b43d8968c299073c5dae6f837258
egaranda <egaranda@e-externas.enaire.es>
2023-01-18 16:31:09 +0100

    Corregido  las referencias de los objetos

--- src/main/java/com/unisys/miapp/AppCadenas.java
+++ src/main/java/com/unisys/miapp/AppCadenas.java
@@ -3,14 +3,16 @@ package com.unisys.miapp;
 public class AppCadenas {
 
     public static void main(String[] args) {
-        String saludo = "Buongiorno bafanculo";
+        // String saludo = "Buongiorno bafanculo"; si declaro asi los objetos no son distintos
+        // String saludo2 = "Buongiorno bafanculo"; si declaro asi los objetos no son distintos
+        String saludo = new String("Buongiorno bafanculo");
         System.out.println("Contenido de variable saludo: "+saludo);
         System.out.println("Longuitud de variable saludo: "+saludo.length());
         System.out.println("Cuarto caracter de variable saludo: "+saludo.substring(3,4));
         System.out.println("Variable saludo en mayusculas: "+saludo.toUpperCase());
         System.out.println("Variable saludo en minusculas: "+saludo.toLowerCase());
         System.out.println("Posicion del caracter l en la variable saludo: "+saludo.indexOf("l"));
-        String saludo2 = "Buongiorno bafanculo";
+        String saludo2 = new String("Buongiorno bafanculo");
         if (saludo == saludo2) {
             System.out.println("Referencias iguales");
         }
